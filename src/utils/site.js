144f52f4339429b46009d6db64c9365984879bf6
mujiarjo3 <mujiarjo3@gmail.com>
2025-07-10 10:50:03 +0700

    Update site.js

--- src/utils/site.js
+++ src/utils/site.js
@@ -4,6 +4,6 @@ export const desk = 'Video Bokep Indo Terbaru Bocil Ngentot Jilbab Smp Mama Vira
 export const terbit = '2025-01-25T00:01:21Z';
 
 export const google = 'zbnqTEiinpHi4_1AKlztwfNnUYyIv1P0ONgFG8QnMz0';
-export const yandex = '41cef1584a63f637';
+export const yandex = 'ed61668fa98a2fcf';
 
 export const index = 'ff81a001-736c-4ff2-bd27-347e993fe42f';
